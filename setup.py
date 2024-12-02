7b57feb4e4ac4351700d7a01bd447d392c19ce5a
Saad Muhammad <142020554+saad-muhammad01@users.noreply.github.com>
2024-12-02 21:34:19 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import pyfiglet
+
+# Create ASCII art with a cool font style
+ascii_art = pyfiglet.figlet_format("SAAD SOFTWARE", font="slant")
+print(ascii_art)
+
+# Print additional messages
+print("All Rights Reserved Copyright 2024")
+print("Operating System setup is successful")
